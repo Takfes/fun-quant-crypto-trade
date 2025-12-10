e8084b51d1be30b0840f359755681139d7946ff4
tfe <Takfes@users.noreply.github.com>
2025-12-10 02:02:59 +0200

    wip portfolio e2e script, colab scripts

--- scripts/portfolio/optimization_e2e_exercise.py
+++ scripts/portfolio/optimization_e2e_exercise.py
@@ -1,43 +1,79 @@
 import numpy as np
+import pandas as pd
 import yfinance as yf
 from matplotlib import pyplot as plt
 
-from src.func import annual_risk_return, plot_assets_density
+START = "2020-01-01"
+END = "2025-11-30"
+RISK_FREE_RATE = 0.0417
+MIN_ALLOCATION_WEIGHT = 0.0
+MAX_ALLOCATION_WEIGHT = 0.5
+NUM_PORTFOLIOS = 10_000
 
-tickers = ["MSFT", "NVDA", "AMZN", "CVX"]
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
 
-rdata = yf.download(tickers, start="2020-01-01", end="2023-07-31")
+sp500 = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies", storage_options=headers)[0]
 
-data = rdata["Adj Close"].copy()
+tickers = [
+    "AAPL",
+    "AMGN",
+    "CAT",
+    "FDX",
+    "GE",
+    "GS",
+    "INTC",
+    "IRM",
+    "JNJ",
+    "JPM",
+    "LMT",
+    "MRK",
+    "MRNA",
+    "MSFT",
+    "MTCH",
+    "PLG",
+    "RTX",
+    "WELL",
+    "WMB",
+    "XOM",
+]
 
-assert data.isnull().sum().sum() == 0
+rawdata = yf.download(tickers, start=START, end=END)
+closedata = rawdata["Close"].copy()
+returnsdata = closedata.pct_change().dropna()
 
-returns = data.pct_change().dropna()
+statsdata = pd.DataFrame({
+    "min": returnsdata.min(),
+    "q99": returnsdata.quantile(0.01),
+    "q95": returnsdata.quantile(0.05),
+    "mean": returnsdata.mean(),
+    "median": returnsdata.median(),
+    "max": returnsdata.max(),
+    "std": returnsdata.std(),
+    "skew": returnsdata.skew(),
+    "kurtosis": returnsdata.kurtosis(),
+    "annual_returns": returnsdata.mean() * 252,
+    "annual_std": returnsdata.std() * np.sqrt(252),
+    "coefficient_of_variation": returnsdata.std() * np.sqrt(252) / (returnsdata.mean() * 252),
+}).T
 
-returns.to_clipboard()
-returns.plot(kind="density", figsize=(10, 6))
-plot_assets_density(returns)
+covmatrix = returnsdata.cov()
+cormatrix = returnsdata.corr()
 
-cov_matrix = returns.cov()
+# Generate random portfolios
+weights_equal = np.ones(len(tickers)) / len(tickers)
+weights_dirichlet = np.random.dirichlet(np.ones(len(tickers)), size=NUM_PORTFOLIOS)
+weights_power = np.random.rand(NUM_PORTFOLIOS, len(tickers))
+weights_power /= weights_power.sum(axis=1)[:, np.newaxis]
+weights = np.vstack([weights_equal, weights_dirichlet, weights_power])
 
-summary = annual_risk_return(returns)
+# Calculate annualized portfolio returns & risk for all portfolios
+portfolio_returns = weights @ statsdata.loc["annual_returns"].values
+portfolio_variance = (weights @ covmatrix * weights).sum(axis=1)
+portfolio_risk = (np.sqrt(portfolio_variance) * np.sqrt(252)).values
 
-summary["FutureReturns"] = summary.Returns.mul(0.75)
+# Plot the portfolio risk-return scatter
+plt.scatter(portfolio_risk, portfolio_returns, c=portfolio_returns / portfolio_risk, marker="o")
 
-noa = returns.shape[1]
-nop = 1_000_000
-
-np.random.seed(1990)
-matrix = np.random.random(noa * nop).reshape(nop, noa)
-weights = matrix / matrix.sum(axis=1, keepdims=True)
-
-Returns = summary.FutureReturns.dot(weights.T)
-
-Risk = np.sqrt((cov_matrix.dot(weights.T).T * weights).sum(axis=1))
-
-plt.figure(figsize=(10, 6))
-plt.scatter(summary.Risk, summary.Returns, c="red", marker="D")
-for i in range(noa):
-    plt.annotate(summary.index[i], xy=(summary.Risk[i], summary.Returns[i]), size=15)
-plt.scatter(Risk, Returns, c=Returns / Risk, marker="o")
-plt.show()
+# np.sqrt((weights_equal @ covmatrix) @ weights_equal.T) * np.sqrt(252)
